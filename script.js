3b49ceb4e3ea007e7a5b02c9fcba3559b1a6ecd0
Yulvonptz <spoiled.21ptz@gmail.com>
2025-07-29 00:38:13 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JS для навигации и кнопок, чтобы они корректно менялись на всех страницах
-document.querySelectorAll('.nav-button').forEach(button => {
-  button.addEventListener('mouseover', function() {
-    button.style.color = '#ff0077';
-  });
-  button.addEventListener('mouseout', function() {
-    button.style.color = '';
-  });
-});
